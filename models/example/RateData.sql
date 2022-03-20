3a7052f0360984c5904318322ca2268ca3ef1812
DEEPANSHU GOYAL <goyaldeepanshu0098@gmail.com>
2022-03-20 12:50:22 +0000

    MODEULES_ADDED

---
+++ models/example/RateData.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+
+   SELECT TO_DATE(Date,'MM/DD/YYYY')  DATE,GROWTH_FACTOR_OF_NEW_DEATHS,CASE_FATALITY_RATE,CASE_RECOVERED_RATE,GROWTH_FACTOR_OF_NEW_CASES
+           FROM FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_DEEPANSHU_GOYAL 
+          
+)
+
+select *
+from source_data
